bc7758e4fb9f8cde7c2d75d8b60362326363f6f6
paulblacklock-authomize <paul.blacklock@authomize.com>
2023-05-04 10:28:51 -0500

    Splunk connector
    Splunk connector

--- Open-Connectors/Connectors/Authomize_Docker_Image/schedule_config.py
+++ Open-Connectors/Connectors/Authomize_Docker_Image/schedule_config.py
@@ -1,5 +1,6 @@
 # schedule_config.py
 scripts_to_schedule = [
-    {'cron': '0 */12 * * *', 'path': '/app/atlassian/atlassian.py'},
+    {'cron': '0 */12 * * *', 'path': '/app/atlassian/atlassian.py'}, # cron for every 12 hours at 0000 % 1200.
+    {'cron': '0 */12 * * *', 'path': '/app/splunk/splunk.py'}  
     # {'cron': '0 */12 * * *', 'path': 'path/to/your/script2.py'}  # add other scripts here
 ]
